68147f17bde325ac1fcb66784d9c8072e612d7c1
William Escobar <wescobarr@gmail.com>
2026-09-22 10:20:03 -0500

    ajuste back 6

--- obj/Debug/net10.0/IglesiaBackend.AssemblyInfo.cs
+++ obj/Debug/net10.0/IglesiaBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IglesiaBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c27c9f73de91562ad1fc5d24774562b048e1b48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+072bf0b5a4676328d56aceb164a21f67bcad9ae8")]
 [assembly: System.Reflection.AssemblyProductAttribute("IglesiaBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IglesiaBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
